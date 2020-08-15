2aa9fdf0c368b05e555bf7c085f297c5f43c481b
Maxwell DeMaio <maxdman321@gmail.com>
2020-08-15 16:06:27 -0400

    Completed add cards functionality, need to implement error handling

--- addCards.js
+++ addCards.js
@@ -1,43 +1,64 @@
-//// Add a card to a given deck with a front/back side
-/// If optional parameter is given (csv file or excel file read all data)
+/// Add a card to a given deck with a front/back side with optional examples
+
+/// If optional parameter is given with initial startup ex) 'node addCards.js newCards.csv'
+/// Then cards will be added from that specified file, rather than manually in the command line
+/// This will work for CSV and Excel files, so long as they have the following headers:
+/// side1, side2, examples
+/// Note: if there are any examples, delimit them with underscores
 
 // Load modules
 var fs = require('fs');
 var readline = require('readline');
+var rl = readline.createInterface(process.stdin, process.stdout);
 
 // Set card deck
 var cardFile = 'exampleDeck.json'
 
-
 console.log("Welcome to Command Line Spaced Repetition (Add cards)!\n" +
     "Here you can add cards to your specified deck:\n" +
     "When prompted for the front side, type what should be displayed.\n" +
-    "When prompted for the back side, type what the answer should be.\n");
+    "When asked for the back side, type what the answer should be.\n" + 
+    "Examples for the card should be delimited by underscores.\n");
 
-// Defining new card 
-let newCard = {
-    side1: "porte",
-    side2: "(nf) door",
-    examples: ["Ouvre la porte!", "Ferme la porte!"]
-};
 
-// Read card deck
-fs.readFile(cardFile, function (err, data) {
-    if (err) throw err;
-    cards = JSON.parse(data);
-    console.log(cards);
+// Get input from user and add card to deck
+rl.question("Front side: ", function (front) {
+    rl.question("Back side: ", function (back) {
+        rl.question("Examples (delimit with underscores): ", function (examples) {
+            // Populate new card
+            // TODO do error checking on front/back/examples
+            let newCard = {};
+            newCard["side1"] = front;
+            newCard["side2"] = back;
+            newCard["examples"] = examples.split("_");
 
-    // Adding new data to cards object 
-    cards.push(newCard); 
+            console.log(`\nFront side: "${front}"\nBack side: "${back}"\nExamples: "${examples}"\n`);
 
-    // Write changes to card deck
-    fs.writeFile(cardFile, JSON.stringify(cards), err => {
-
-        // Checking for errors 
-        if (err) throw err;
-
-        console.log("Done writing"); // Success 
-    }); 
-
+            updateDeck(newCard);
+            rl.close();
+        });
+    });
 });
 
+function updateDeck(newCard) {
+    console.log("Adding new card...");
+    fs.readFile(cardFile, function (err, data) {
+
+        if (err) throw err;
+        cards = JSON.parse(data);
+
+        // TODO check if front side already exists in cards
+
+        // Adding new data to card deck object
+        cards.push(newCard);
+
+        // Write changes to card deck
+        fs.writeFile(cardFile, JSON.stringify(cards), err => {
+
+            // Checking for errors 
+            if (err) throw err;
+
+            console.log("Card added"); // Success 
+        });
+    });
+};
